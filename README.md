ac0e13fa94f8befadb9303be5f006187db59ef7c
vyomaaverse <hyperiongoldds@gmail.com>
2024-01-31 13:21:18 +0530

    Updated ReadME

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Phishing Detection using URL Analysis
 
-This repository contains code and resources for a research project focused on phishing detection using URL analysis. The goal of this project is to develop a machine learning model that can accurately classify URLs as either legitimate or phishing.
+## Introduction
+
+This repository contains code and resources for a research project focused on phishing detection using URL analysis. The goal of this project is to leverage machine learning techniques to develop a highly accurate model that can classify URLs as either legitimate or phishing. By analyzing various features of URLs, such as domain reputation, URL length, and presence of suspicious keywords, the model aims to provide effective protection against phishing attacks. Through the use of advanced machine learning algorithms, this project aims to enhance the security of online users and mitigate the risks associated with phishing attempts.
 
 ## Table of Contents
 
@@ -12,15 +14,39 @@ This repository contains code and resources for a research project focused on ph
 
 ## Introduction
 
-Phishing attacks are a common form of cybercrime where attackers attempt to deceive users into revealing sensitive information such as passwords, credit card details, or personal data. This project aims to leverage URL analysis techniques to identify and classify phishing URLs.
+Phishing attacks are a prevalent and persistent form of cybercrime, posing a significant threat to individuals, organizations, and even governments. These attacks involve malicious actors attempting to deceive unsuspecting users into revealing sensitive information, such as passwords, credit card details, or personal data. The consequences of falling victim to a phishing attack can be severe, ranging from financial loss to identity theft and reputational damage.
+
+To combat this ever-evolving threat landscape, this research project focuses on leveraging advanced URL analysis techniques to identify and classify phishing URLs. By analyzing various features of URLs, such as domain reputation, URL length, presence of suspicious keywords, and other relevant indicators, the project aims to develop a highly accurate machine learning model. This model will be capable of distinguishing between legitimate URLs and phishing URLs, providing effective protection against phishing attacks.
+
+The ultimate goal of this project is to enhance the security of online users and mitigate the risks associated with phishing attempts. By developing a robust and reliable phishing detection model, individuals, businesses, and organizations can proactively defend against phishing attacks, safeguarding their sensitive information and preserving their digital trust.
 
 ## Installation
 
 To use the code in this repository, follow these steps:
 
-1. Clone the repository: `git clone https://github.com/your-username/your-repo.git`
+1. Clone the repository: `git clone https://github.com/vyomaaverse/phishing-research.git`
 2. Install the required dependencies: `pip install -r requirements.txt`
 
 ## Usage
 
-To train and evaluate the phishing detection model, run the following command:
+To train and evaluate the phishing detection model, follow these steps:
+
+1. Open the terminal and navigate to the project directory.
+2. Run the following command to train the model:
+    ```
+    python hybrid_RF.py
+    ```
+Make sure you have the required dependencies installed before running the commands. For more details, refer to the [Installation](#installation) section.
+## Contributing
+
+Contributions are welcome! If you would like to contribute to this project, please follow these guidelines:
+
+1. Fork the repository
+2. Create a new branch: `git checkout -b my-feature`
+3. Make your changes and commit them: `git commit -m 'Add some feature'`
+4. Push to the branch: `git push origin my-feature`
+5. Submit a pull request
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- hybrid_RF.py
+++ hybrid_RF.py
@@ -5,10 +5,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, make_scorer, precision_score, recall_score,f1_score
 
 # Load the data
-df = pd.read_csv('first5000.csv')
+df = pd.read_csv('features_extracted_01-01-24_dots_path_dots_dom_token.csv')
 
 # Rule-based classification
-rule_based_features = ['brand_path', 'brand_subdomain', 'brand_substring_dom', 'brand_typo_dom', 'fake_tld']
+rule_based_features = ['brand_path', 'brand_subdomain', 'brand_substring_dom','brand_typo_dom', 'fake_tld']
 rule_based_mask = df[rule_based_features].sum(axis=1) > 0
 df['rule_based_prediction'] = rule_based_mask.astype(int)
 
@@ -27,64 +27,60 @@ print(f"Accuracy: {rule_based_accuracy:.4f}")
 print(f"Precision: {rule_based_precision:.4f}")
 print(f"Recall: {rule_based_recall:.4f}")
 print(f"F1 score:{rule_based_f1:.4f}")
-
+rule_based_data.to_csv('rule_results_01-01-24.csv', index=False)
 # Machine learning classification with cross-validation
+
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+
 X = ml_data.drop(['url', 'label'], axis=1)
 y = ml_data['label']
 
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
-# print(y.value_counts())
-# cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-scoring = {'accuracy' : 'accuracy','precision': 'precision', 'recall': 'recall'}
+# Define the metrics to be calculated during cross-validation
+scoring = {'accuracy': 'accuracy', 'precision': 'precision', 'recall': 'recall', 'f1': 'f1'}
 
-# Initialize the cross-validation method
-rskf = RepeatedStratifiedKFold(n_splits=5, n_repeats=10, random_state=42)
+# Number of repeats
+num_repeats = 10
 
-# Initialize arrays to hold the predictions and true labels
-all_predictions = []
+ml_cv_predictions = []
+ml_cv_scores = []
+ml_rule_accuracy=[]
+ml_rule_precision=[]
+ml_rule_recall=[]
 
-# Manually implement the cross-validation loop
-for train_index, test_index in rskf.split(X, y):
-    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+for _ in range(num_repeats):
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42) 
+    ml_cv_predictions.clear()
+    # Perform cross-validation on the entire dataset
+    cv_results = cross_val_predict(clf, X, y, cv=cv)
+    scores = cross_validate(clf, X, y,  cv=StratifiedKFold(n_splits=5, shuffle=True, random_state=42), scoring=scoring, return_train_score=False)
+    ml_cv_predictions.extend(cv_results)
+
+
+    ml_cv_scores.append(scores)
+    ml_rule_predictions= pd.concat([rule_based_data['rule_based_prediction'], pd.Series(ml_cv_predictions)])
+    ml_rule_labels=pd.concat([rule_based_data['label'], y])
+    ml_rule_accuracy.append(accuracy_score(ml_rule_labels, ml_rule_predictions))
+    ml_rule_precision.append(precision_score(ml_rule_labels, ml_rule_predictions))
+    ml_rule_recall.append(recall_score(ml_rule_labels, ml_rule_predictions))
     
-    # Fit the classifier and make predictions
-    clf.fit(X_train, y_train)
-    predictions = clf.predict(X_test)
-    
-    # Store the predictions and true labels
-    all_predictions.extend(predictions)
 
-all_predictions = np.array(all_predictions)
-all_predictions = all_predictions.reshape((-1, len(X)))
-average_predictions = all_predictions.mean(axis=0)
+"""ml_predictions_df = ml_data[['url', 'label']].copy()  # Copy features and actual labels
+ml_predictions_df['ml_prediction'] = ml_cv_predictions  # Add machine learning predictions
 
-cv_results = pd.Series(average_predictions, index=X.index)
+# Save to CSV file
+ml_predictions_df.to_csv('ml_predictions_01-01-24.csv', index=False)"""
 
-threshold = 0.5
-cv_results = (cv_results > threshold).astype(int)
+# Print test metrics
+print(f"Mean Test Accuracy: {np.mean([score['test_accuracy'].mean() for score in ml_cv_scores]):.4f}")
+print(f"Mean Test Precision: {np.mean([score['test_precision'].mean() for score in ml_cv_scores]):.4f}")
+print(f"Mean Test recall: {np.mean([score['test_recall'].mean() for score in ml_cv_scores]):.4f}")
 
-ml_cv_scores = cross_validate(clf, X, y, cv=rskf, scoring=scoring, return_train_score=True)
+#print overall metrics
+print(f"Overall Accuracy: { (sum(ml_rule_accuracy) / len(ml_rule_accuracy)):.4f}")
+print(f"Overall Precision: { (sum(ml_rule_precision) / len(ml_rule_precision)):.4f}")
+print(f"Overall Recall: { (sum(ml_rule_recall) / len(ml_rule_recall)):.4f}")
 
-rule_based_data.to_csv('rule_results.csv', index=False)
 
-# Extract and print the cross-validation results
-print("Machine Learning Metrics:")
-print("Mean Training Accuracy: {:.3f}".format(np.mean(ml_cv_scores['test_accuracy'])))
-print("Mean Training Precision: {:.3f}".format(np.mean(ml_cv_scores['test_precision'])))
-print("Mean Training Recall: {:.3f}".format(np.mean(ml_cv_scores['test_recall'])))
 
-# Overall metrics (combining rule-based and machine learning predictions)
-overall_predictions = pd.concat([rule_based_data['rule_based_prediction'], cv_results])
-overall_labels = pd.concat([rule_based_data['label'], y])
 
-overall_accuracy = accuracy_score(overall_labels, overall_predictions)
-overall_precision = precision_score(overall_labels, overall_predictions)
-overall_recall = recall_score(overall_labels, overall_predictions)
-overall_f1=f1_score(overall_labels, overall_predictions)
 
-print("\nOverall Metrics:")
-print(f"Overall Accuracy: {overall_accuracy:.4f}")
-print(f"Overall Precision: {overall_precision:.4f}")
-print(f"Overall Recall: {overall_recall:.4f}")
-print(f"f1_score:{overall_f1:.4f}")
